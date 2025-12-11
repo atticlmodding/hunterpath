a8c64ff8922f0a4de15fb534ed0429305df419c9
atticl <radix.julian.owen@gmail.com>
2025-12-11 14:27:16 +0100

    Initial commit

---
+++ src/client/java/hunterpath/HunterPathClient.java
@@ -0,0 +1,10 @@
+package hunterpath;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HunterPathClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
